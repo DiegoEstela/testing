6b367113678c1e32af28432f661b07a0b8e84069
Diego Estela Ezequiel <destela@knowmadmood.com>
2023-09-22 14:58:01 +0200

    use renderComponents

--- form-app/src/UserList.test.js
+++ form-app/src/UserList.test.js
@@ -1,16 +1,34 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+function renderComponents(){
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "diego", email: "diego@diego.com" },
   ];
-  render(<UserList users={users} />);
+   render(<UserList users={users} />);
 
-  const rows = within(screen.getByTestId("users")).getAllByRole('row')
+   return{
+    users
+   }
+}
 
+test("render one row per user", () => {
+  renderComponents()
+  const rows = within(screen.getByTestId("users")).getAllByRole("row")
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+
+  const {users} = renderComponents()
+  for (let user of users){
+    const name = screen.getByRole('cell', {name : user.name})
+    const email = screen.getByRole('cell', {name : user.email})
+    
+    expect(name).toBeInTheDocument()
+    expect(email).toBeInTheDocument()
+  }
+
+
+});
