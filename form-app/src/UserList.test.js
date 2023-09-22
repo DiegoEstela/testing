0e58a4cd6f48a8944ae710578e3d4de69251f1e1
Diego Estela Ezequiel <destela@knowmadmood.com>
2023-09-22 10:31:55 +0200

    we use the data-testid

---
+++ form-app/src/UserList.test.js
@@ -0,0 +1,16 @@
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
+
+test("render one row per user", () => {
+  const users = [
+    { name: "jane", email: "jane@jane.com" },
+    { name: "diego", email: "diego@diego.com" },
+  ];
+  render(<UserList users={users} />);
+
+  const rows = within(screen.getByTestId("users")).getAllByRole('row')
+
+  expect(rows).toHaveLength(2);
+});
+
+test("render the email and name of each user", () => {});
